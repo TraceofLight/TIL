030e6ea11bdc1d80d21420c9b6c4d4b64f812a6b
TraceofLight <98262849+TraceofLight@users.noreply.github.com>
2022-07-23 23:47:14 +0900

    BOJ Q
    solve 1004

--- Algorithm/BOJ Solution/001004.py
+++ Algorithm/BOJ Solution/001004.py
@@ -1,29 +1,33 @@
 def distance(a, b, c, d) :
-    result = ((c - a) ** 2 + (d - b)** 2) ** (1 / 2)
+    a, b, c, d = int(a), int(b), int(c), int(d)
+    result = ((c - a) ** 2) + ((d - b)** 2)
     return int(result)
 
 testCase = int(input())
-planet_info_ = []
 planet_info = []
 satisfy_option = 0
 output = []
 for i in range(testCase) :
     x1, y1, x2, y2 = map(int,input().split())
-    print(x1)
     orbit_number = int(input())
     for j in range(orbit_number) :
-        planet_info_ = list(map(int,input().split()))
-        planet_info.append(planet_info_)
-    print(planet_info)
+        planet_info.append(list(map(int,input().split())))
+    counter = 0
     for j in range(orbit_number) :
-        if distance(x1, y1, planet_info[j][0], planet_info[j][1]) < planet_info[j][2] :
-            satisfy_option += 1
-        if distance(x2, y2, planet_info[j][0], planet_info[j][1]) < planet_info[j][2] :
-            satisfy_option += 1
-        if distance(x1, y1, planet_info[j][0], planet_info[j][1]) < planet_info[j][2] and distance(x2, y2, planet_info[j][0], planet_info[j][1]) < planet_info[j][2] :
-            satisfy_option -= 2
+        cx = planet_info[j][0]
+        cy = planet_info[j][1]
+        cr = planet_info[j][2]
+        if distance(x1, y1, cx, cy) < cr**2 :
+            counter += 1
+        if distance(x2, y2, cx, cy) < cr**2 :
+            counter += 1
+        if distance(x1, y1, cx, cy) < cr**2 and distance(x2, y2, cx, cy) < cr**2 :
+            counter += -2
+        satisfy_option += counter
+        counter = 0
     output.append(satisfy_option)
     satisfy_option = 0
+    planet_info = []
 
 for i in range(testCase) :
     print(output[i])
