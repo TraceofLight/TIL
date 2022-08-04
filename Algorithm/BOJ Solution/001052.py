cf7abf03728f497baa42f6180a620b3faa9ff4ea
TraceofLight <rickyjun45@gmail.com>
2022-08-04 17:49:36 +0900

    BOJ Q start
    move workspace

---
+++ Algorithm/BOJ Solution/001052.py
@@ -0,0 +1,11 @@
+n, k = list(map(int,input().split()))
+output = []
+cup = 0
+while k > output :
+    if n == output :
+        while True :
+            pass
+    output.append(n // 2 + n % 2)
+
+def findcup(n, cup, output) :
+    pass
