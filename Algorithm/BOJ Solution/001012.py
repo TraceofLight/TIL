0b8b721a15057273e455b2ba83ad7d93b93ab775
TraceofLight <rickyjun45@gmail.com>
2022-08-16 01:21:04 +0900

    BOJ Q
    solve several question

--- Algorithm/BOJ Solution/001012.py
+++ Algorithm/BOJ Solution/001012.py
@@ -3,41 +3,38 @@ from collections import deque
 
 testcase = int(sys.stdin.readline())
 output = []
-cabbage_list = []
+
 for case in range(testcase):
-    width, height, amount = list(map(int, sys.stdin.readline().split()))
-    for cabbage in range(amount):
-        idx_x, idx_y = map(int, sys.stdin.readline().split())
-        if idx_x < 0 or idx_x >= width or idx_y < 0 or idx_y >= height:
-            continue
-        else:
-            cabbage_list.append([idx_x, idx_y])
+    cabbage_list = []
+    width, height, number = list(map(int, sys.stdin.readline().split()))
+    for cabbage in range(number):
+        cabbage_list.append(list(map(int, sys.stdin.readline().split())))
 
     # 그래프 생성
     visited = [False for _ in cabbage_list]
     directions = [[-1, 0], [0, 1], [1, 0], [0, -1]]
-    graph = [[] for _ in cabbage_list]
-    for idx1 in range(amount):
-        for idx2 in range(amount):
+    graph = [set() for _ in cabbage_list]
+    for idx1 in range(number):
+        for idx2 in range(number):
             if idx1 == idx2:
                 continue
             for direction in directions:
                 if ([cabbage_list[idx1][0] + direction[0], cabbage_list[idx1][1] + direction[1]] 
                     == [cabbage_list[idx2][0], cabbage_list[idx2][1]]):
-                    graph[idx1].append(idx2)
-                    break
+                    graph[idx1].add(idx2)
+                    graph[idx2].add(idx1)
 
     progress_que = deque([])
     result = 0
     # 그래프 내에서 BFS
-    for idx in range(amount):
+    for idx in range(number):
         if visited[idx]:
             continue
         else:
             progress_que.append(idx)
             visited[idx] = True
             while progress_que:
-                progress_idx = progress_que.pop()
+                progress_idx = progress_que.popleft()
                 for point in graph[progress_idx]:
                     if not visited[point]:
                         progress_que.append(point)
@@ -46,19 +43,3 @@ for case in range(testcase):
     output.append(result)
 
 print(*output, sep='\n')
-
-'''
-    # 그래프 내에서 DFS
-    for idx in range(amount):
-        if visited[idx]:
-            continue
-        else:
-            progress_que.append(idx)
-            while progress_que != deque([]):
-                progress_idx = progress_que.popleft()
-                visited[progress_idx] = True
-                for cabbage_point in graph[progress_idx]:
-                    if not visited[cabbage_point]:
-                        progress_que.append(cabbage_point)
-            result += 1
-'''
--- Algorithm/BOJ Solution/024444.py
+++ Algorithm/BOJ Solution/024444.py
@@ -3,8 +3,55 @@ from collections import deque
 
 nodes, lines, start_point = list(map(int, sys.stdin.readline().split()))
 line_list = []
-node_list = [set() for _ in range(nodes)]
-visited = []
+line_list_raw = [set() for _ in range(nodes)]
+visited = [False for _ in range(nodes)]
+
+for _ in range(lines):
+    start_node, end_node = map(int, sys.stdin.readline().split())
+    line_list_raw[start_node - 1].add(end_node)
+    line_list_raw[end_node - 1].add(start_node)
+
+line_list = [sorted(list(element)) for element in line_list_raw]
 
 progress_que = deque([])
+result = []
 
+progress_que.append(start_point)
+visited[start_point - 1] = True
+result.append(start_point)
+while progress_que != deque([]):
+    progress_idx = progress_que.popleft() - 1
+    for element in line_list[progress_idx]:
+        if not visited[element - 1]:
+            progress_que.append(element)
+            visited[element - 1] = True
+            result.append(element)
+
+visit_data_raw = list(enumerate(result))
+visit_dict = {data[1]: data[0] for data in visit_data_raw}
+
+for node in range(1, nodes + 1):
+    if visit_dict.get(node) == None:
+        print(0)
+    else:
+        print(visit_dict.get(node) + 1)
+
+'''
+loop_list = []
+loop_list.append(start_point)
+for idx in range(1, nodes + 1):
+    if idx == start_point:
+        continue
+    loop_list.append(idx)
+
+for idx in loop_list:
+    if not idx in visited:
+        progress_que.append(idx)
+        visited.append(idx)
+        while progress_que != deque([]):
+            progress_idx = progress_que.popleft() - 1
+            for element in node_list[progress_idx]:
+                if element not in visited:
+                    progress_que.append(element)
+                    visited.append(element)
+'''
