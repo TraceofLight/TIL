ea2e63b7c5fe786ad6107bc39f84e99df545c972
TraceofLight <rickyjun45@gmail.com>
2022-08-23 18:02:13 +0900

    BOJ Q
    solve 1541

--- Algorithm/BOJ Solution/001541.py
+++ Algorithm/BOJ Solution/001541.py
@@ -1,21 +1,31 @@
 import sys
-import math
-formula_raw = sys.stdin.readline().strip()
-stack1 = []
 
+formula_raw = sys.stdin.readline().strip()
 formula_raw = formula_raw.replace('-', '*-')
 formula_raw = formula_raw.replace('+', '*')
-
 formula_raw = list(formula_raw.split('*'))
-formula = []
 
+formula = []
 get_minus = []
 counter = 0
+
 for chr in formula_raw:
     formula.append(int(chr))
     counter += 1
     if int(chr) < 0:
         get_minus.append(counter)
 
-for idx in range(1):
-    pass
+result = 0
+is_minus = False
+for value in formula:
+    if value < 0:
+        is_minus = True
+        result += value
+        continue
+    else:
+        if is_minus:
+            result -= value
+        else:
+            result += value
+
+print(result)
